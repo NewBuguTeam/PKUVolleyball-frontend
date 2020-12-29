c69534247437eeb547a1e9f5591cb3accd069227
MrRobertYuan <634954435@qq.com>
2020-12-29 10:10:58 +0800

    增加了基本的保存和加载功能

--- PKUVolleyball/pages/confirmPoints/confirmPoints.js
+++ PKUVolleyball/pages/confirmPoints/confirmPoints.js
@@ -1,5 +1,6 @@
 // pages/confirmPoints/confirmPoints.js
 var app = getApp()
+var storageTime = 30 * 60
 const util = require("../../utils/util")
 Page({
 
@@ -10,16 +11,17 @@ Page({
     maxRound: 5,
     nameA: "信科",
     nameB: "医学",
-    curRound: 1,
+    curRound: 0,
     curPointA: 0,
     curPointB: 0,
     allPoints: [],
-    curDuration: "",
+    curProcedure: "",
     showTimeTicker: false,
     minute:"00",
     second:"00",
     microSecond:"00",
-    timer:null
+    timer:null,
+    startTime:""
   },
 
   ruleAction:function(){
@@ -27,35 +29,105 @@ Page({
     that.setData({
       ruleDesImageHidden: false,
     })
- },
+  },
+
+  formatString: function(value){
+    if(Number(value) < 10)
+      return "0" + value
+    else
+      return value
+  },
+
+  calcuDeltaTime: function(dateTime1, dateTime2){
+    let time1 = dateTime1.split(' ')[1].split(':')
+    let time2 = dateTime2.split(' ')[1].split(':')
+    console.log(time1)
+    console.log(time2)
+    let ans = []
+    ans[2] = Number(time1[2]) - Number(time2[2])
+    if(ans[2] < 0){
+      ans[2] += 60
+      time1[1] = Number(time1[1]) - 1
+    }
+    ans[1] = Number(time1[1]) - Number(time2[1])
+    if(ans[1] < 0){
+      ans[1] += 60
+      time1[0] = Number(time1[0]) - 1
+    }
+    ans[0] = Number(time1[0]) - Number(time2[0])
+    if(ans[0] < 0){
+      console.log("不允许跨日期的比赛")
+      ans[0] += 24
+    }
+    let ansString = this.formatString(ans[0]) + ":" + this.formatString(ans[1])
+    + ":" + this.formatString(ans[2])
+    return ansString
+  },
 
   recordPoints: function(){
+    let curTime = util.formatTime(new Date())
+    let deltaTime = this.calcuDeltaTime(curTime, this.data.startTime)
+    let point='allPoints['+this.data.curRound+'].point'
+    let procedure='allPoints['+this.data.curRound+'].procedure'
+    let duration='allPoints['+this.data.curRound+'].duration'
+    let startTime='allPoints['+this.data.curRound+'].startTime'
     this.setData({
-      'allPoints[curRound].value': curDuration
+      [point]: this.data.curPointA + ":" + this.data.curPointB,
+      [procedure]: this.data.curProcedure,
+      [duration]: deltaTime,
+      [startTime]: this.data.startTime
+    })
+    util.putStorage("allPoints", this.data.allPoints, storageTime)
+    console.log(this.data.allPoints)
+  },
+
+  loadPoint:function(){
+    this.setData({
+      allPoints: util.getStorage("allPoints")
+    })
+    let curRound = this.data.curRound
+    if(this.data.allPoints[curRound] == undefined){
+      let curTime = util.formatTime(new Date())
+      this.setData({
+        curPointA: 0,
+        curPointB: 0,
+        curProcedure: "",
+        startTime:curTime
+      })
+      return
+    }
+    let point = this.data.allPoints[curRound].point.split(':')
+    this.setData({
+      curPointA: Number(point[0]),
+      curPointB: Number(point[1]),
+      curProcedure: this.data.allPoints[curRound].procedure,
+      startTime: this.data.allPoints[curRound].startTime
     })
   },
 
   teamAScore:function(e){
     this.setData({
       curPointA: this.data.curPointA + 1,
-      curDuration: this.data.curDuration + 'A'
+      curProcedure: this.data.curProcedure + 'A'
     })
-    console.log(this.data.curDuration)
+    console.log(this.data.curProcedure)
   },
 
   teamBScore:function(e){
     this.setData({
       curPointB: this.data.curPointB + 1,
-      curDuration: this.data.curDuration + 'B'
+      curProcedure: this.data.curProcedure + 'B'
     })
-    console.log(this.data.curDuration)
+    console.log(this.data.curProcedure)
   },
 
   chooseCurRound:function(e){
     console.log(e.currentTarget.dataset.index)
+    this.recordPoints()
     this.setData({
       curRound: e.currentTarget.dataset.index
     })
+    this.loadPoint()
   },
 
   setInterval: function () {
@@ -156,13 +228,13 @@ Page({
     if(item == 'A'){
       this.setData({
         pauseTimeA : this.data.pauseTimeA + 1,
-        curDuration: this.data.curDuration + 'C'
+        curProcedure: this.data.curProcedure + 'C'
       })
     }
     else{
       this.setData({
         pauseTimeA : this.data.pauseTimeB + 1,
-        curDuration: this.data.curDuration + 'D'
+        curProcedure: this.data.curProcedure + 'D'
       })
     }
     this.startTimeTicker(30)
@@ -174,13 +246,18 @@ Page({
       title: '提示',
       content: '是否确认提交？',
       success (res){
+        self.recordPoints()
         util.showMassage("提交成功！")
         self.startTimeTicker(180)
+        let curTime = util.formatTime(new Date())
         self.setData({
           curRound: self.data.curRound + 1,
           curPointA: 0,
-          curPointB: 0
+          curPointB: 0,
+          curProcedure: "",
+          startTime:curTime
         })
+        console.log(self.data.startTime)
       },
       fail:function(res){
 
@@ -192,6 +269,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let curTime = util.formatTime(new Date())
+    this.setData({
+      startTime: curTime
+    })
   },
 
   /**
